ae27b80208796132b8428992e4fff0d3827649a6
alefhsousa <alefh.sousa@gmail.com>
2021-05-26 20:37:37 -0300

    setup do projeto para dinâmica de dojo

---
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/Ot4DojoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupedu.dojo.ot4dojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ot4DojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ot4DojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupedu/dojo/ot4dojo/Ot4DojoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupedu.dojo.ot4dojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ot4DojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
